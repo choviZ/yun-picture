9e81955378b2cef3b7035b0b29f3fc40c1405170
choviZ <702381764@qq.com>
2025-02-03 21:51:24 +0800

    前端初始化

---
+++ picture-frontend/src/pages/CreatePicture.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+创建图片
+</template>
+
+<style scoped>
+
+</style>
---
+++ picture-frontend/src/pages/HomePage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  主页<br />
+  主页<br />主页<br />
+  主页<br />
+  主页<br />主页<br />
+
+  主页
+</template>
+
+<style scoped></style>
